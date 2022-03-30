beaef15725b2fa68d09549560f5f4567906a8c98
lucag322 <97728486+lucag322@users.noreply.github.com>
2022-03-30 17:52:29 +0200

    Add files via upload

--- montheme/contacte.php
+++ montheme/contacte.php
@@ -14,10 +14,16 @@
 
     </div>
     <div class="row mb-5">
-      <div class="col-md-4 text-end">
-        <h3>Notre agence :</h3>
+      <div class="col-md-6">
+        <div style="width: 50%;margin-left: auto">
+            <h3>Notre agence :</h3>
+            <p >
+              adressse : 25 Pl. du Général de Gaulle, 91860 Épinay-sous-Sénart<br><br>
+              tel : 06 05 04 07 08
+            </p>
+        </div>
     </div>
-    <div class="col-md-6 text-end">
+    <div class="col-md-4 text-end">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2633.6122313182473!2d2.5130799157712658!3d48.693778020236955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e60a0a25153e15%3A0xe8695b8457316a54!2sAuto-%C3%A9cole%20Mauboussin!5e0!3m2!1sfr!2sfr!4v1648561796213!5m2!1sfr!2sfr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   </div>
